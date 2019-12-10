9078ac01a72b371c0c4a3f811e70d8eb58695a86
Mounir Hader <mounir.hader@MacBook-Pro-3.local>
2019-12-10 15:37:16 +0100

    ffiothub

--- combined.py
+++ combined.py
@@ -11,16 +11,16 @@ import threading
 
 # Bluetooth devices to read data from
 macs = ["DC:07:6C:EF:50:AF", "CF:EF:4C:B9:98:7B", "F0:D1:20:03:81:29"]
+locations = ["office", "ff", "hr"]
 
 # Device-specific shared access signature tokens for azure iot hub
 sas_tokens = [
-    "HostName=future-facts-iothub.azure-devices.net;DeviceId=ruuvitag-1;SharedAccessKey=05GB4CxnEYIje8sPI6HQsVcR3m0XFTvNIMBFzXwj5X4=",
-    "HostName=future-facts-iothub.azure-devices.net;DeviceId=ruuvitag-2;SharedAccessKey=MWP1riNi4+ZSD8K28FzdQn9vD1PCYovk8hNsw1ff8nA=",
-    "HostName=future-facts-iothub.azure-devices.net;DeviceId=ruuvitag-3;SharedAccessKey=TDpfGpLLFpzyxiMFG940HJ1D/GaK9qno5QuMuJcsxpU=",
+    "HostName=future-facts-iothub.azure-devices.net;DeviceId=ruuvitag-1;SharedAccessKey=05GB4CxnEYIje8sPI6HQsVcR3m0XFTvNIMBFzXwj5X4="
 ]
 
 # IoT hub message format
-MSG_TXT = '{{"timestamp": {timestamp}, "device_id": {device_id}, "temperature": {temperature},"humidity": {humidity}}}'
+MSG_TXT = '{{"timestamp": {timestamp}, "device_id": {device_id}, "temperature": {temperature}, \
+          "humidity": {humidity}, "location": {location}}}'
 
 # configure log settings
 log = logging.getLogger("ruuvitag_sensor")
@@ -33,41 +33,47 @@ logging.basicConfig(
 
 
 def iothub_client_init(device):
-    # Create an IoT Hub client
     client = IoTHubDeviceClient.create_from_connection_string(device)
+    client.connect()
     return client
 
 
 def iothub_client_run():
-    log.info("Starting script")
     # IoT hub device clients
-    clients = [iothub_client_init(x) for x in sas_tokens]
+    client = iothub_client_init(sas_tokens[0])
     sensorConnection = RuuviTagSensor()
-    log.info("Devices successfully connected to IoT hub")
 
-    print("Devices now start sending messages to IoT hub, press Ctrl-C to exit")
     # Initialize sensor states
     states = {x: {"temperature": None, "humidity": None} for x in macs}
     # Sensor read loop
     starttime = time.time()
+    counter = 0
     while True:
+        counter += 1
         # scan for devices
         data = sensorConnection.get_data_for_sensors(macs=macs, search_duratio_sec=4)
         found_macs = data.keys()
+        pulltime = time.strftime("%Y-%m-%d %H:%M:%S")
         # update states
         for i in range(len(found_macs)):
             states[found_macs[i]]["temperature"] = data[found_macs[i]]["temperature"]
             states[found_macs[i]]["humidity"] = data[found_macs[i]]["humidity"]
+
         # generate messages
         for i in range(len(macs)):
             msg_txt_formatted = MSG_TXT.format(
                 device_id=i + 1,
                 temperature=states[macs[i]]["temperature"],
                 humidity=states[macs[i]]["humidity"],
-                timestamp=time.strftime("%Y-%m-%d %H:%M:%S")
+                location=locations[i]
+                timestamp=pulltime
             )
             # send messages to IoT hub
-            clients[i].send_message(Message(msg_txt_formatted))
+            client.send_message(Message(msg_txt_formatted))
+
+        if counter % 100 == True:
+            client.disconnect()
+            client.connect()
 
         time.sleep(5.0 - ((time.time() - starttime) % 5))
 
